bdedf67948b65a6e28bf0288474d7cb75961935a
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-05-21 19:16:55 -0400

    question displays on screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,9 @@
 $(".btn-primary").on("click", function() {
     // remove button from view 
     $(".btn-primary").remove(); 
-    // call get question? 
+    // log to console
+    console.log("user clicked start"); 
+    trivia.getQuestion(); 
 
 })
 
@@ -17,46 +19,56 @@ $(".btn-primary").on("click", function() {
 
 // GAME VARIABLE WITH METHODS
 // declare Game variable as an object with the following properties: 
-var trivia = [
-    // questions
-    questions = "",  
-    // current question
-    currentQuestion = "", 
-    // correct answers 
-    correctGuesses = 0, 
-    // incorrect answers 
-    incorrectGuesses = 0, 
-    // timeouts 
-    timeOuts = 0, 
-    // QUESTIONS OBJECT WHICH INCLUDES AN ARRAY OF 
-    questions = [{
-        // question text
-        questionText = "According to Chandler, what scares the bejeezus out of him?", 
-        questionAnswer = ["Clowns", "Another Scary thing", "one more", "Michael Flatley, Lord of the Dance"], 
-        answer = "Michael Flatley, Lord of the Dance", 
-    }, 
-    {
-        questionText = "Monica and Ross had a grandmother who died. Chandler and Joey went to her funeral. Name that grandmother.", 
-        questionAnswer = ["Althea", "Dorothy", "Agnes", "Doris"], 
-        answer = "Althea", 
-    }, 
-    ],
-]
-    //METHODS 
-    // COUNTER
-        // set the counter 
 
-    // GET QUESTION 
+
+var trivia = {
+    // current question
+    currentQuestion: "", 
+    // correct answers 
+    correctGuesses: 0, 
+    // incorrect answers 
+    incorrectGuesses: 0, 
+    // timeouts 
+    timeOuts: 0, 
+    
+    // QUESTIONS OBJECT WHICH INCLUDES AN ARRAY OF 
+    questions: [
+        {
+            // question text
+            questionText: "According to Chandler, what scares the bejeezus out of him?", 
+            // question answers array
+            questionAnswer: ["Clowns", "Another Scary thing", "one more", "Michael Flatley, Lord of the Dance"], 
+            // correct answer 
+            answer: "Michael Flatley, Lord of the Dance" 
+        }, 
+        {
+            questionText: "Monica and Ross had a grandmother who died. Chandler and Joey went to her funeral. Name that grandmother.", 
+            questionAnswer: ["Althea", "Dorothy", "Agnes", "Doris"], 
+            answer: "Althea"
+        }, 
+    ], 
+
+// GET QUESTION METHOD
+    getQuestion: function () {
+    
         // set the counter 
         // display counter on screen 
         // display the question on the screen 
+        $(".question-display").html("<p>" + this.questions[0].questionText + "</p>");  
         // display answer buttons on the screen 
             // if correct answer call correct method
             // else incorrect call incorrect method 
             // else timeout call timeout method 
         // is last question? 
             // yes, call results 
-            // no, call next question 
+            // no, call next question
+    }
+
+}; 
+ 
+    //METHODS 
+    // COUNTER
+        // set the counter 
 
     // NEXT QUESTION 
         // reset the counter 
