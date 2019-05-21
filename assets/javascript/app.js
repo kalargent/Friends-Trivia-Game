c475ee40f8c61a0bc6ddcd8dbf3c24c2bfae89b2
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-05-21 17:27:32 -0400

    pseudocode finished for now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,9 @@
 // ON CLICK EVENT FOR START BUTTON TO GO AWAY 
 
+// ON CLICK FOR RESET BUTTON 
+
+// ON CLICK FOR ANSWER BUTTONS
+
 
 // GAME VARIABLE WITH METHODS
 // declare Game variable as an object with the following properties: 
@@ -20,14 +24,33 @@
         // set the counter 
 
     // GET QUESTION 
-        // 
+        // set the counter 
+        // display counter on screen 
+        // display the question on the screen 
+        // display answer buttons on the screen 
+            // if correct answer call correct method
+            // else incorrect call incorrect method 
+            // else timeout call timeout method 
+        // is last question? 
+            // yes, call results 
+            // no, call next question 
 
     // NEXT QUESTION 
-        //
+        // reset the counter 
+        // display counter on screen 
+        // display the question on the screen 
+        // display answer buttons on the screen 
+            // if correct answer call correct method
+            // else incorrect call incorrect method 
+            // else timeout call timeout method 
+        // is last question? 
+            // yes, call results 
+            // no, call next question 
 
     // CORRECT ANSWER 
         //increments answer var by 1
         // displays a screen saying you were correct and <xx> was right
+        // screen times out in 3 second 
         // checks if that was the last question 
             // if last: call results method 
             // else: call next question 
@@ -35,6 +58,7 @@
     // INCORRECT ANSWER 
         //increments incorrect answer var by 1
         // displays a screen saying you were correct and <xx> was right
+        // screen times out in 3 second 
         // checks if that was the last question 
             // if last: call results method 
             // else: call next question 
@@ -42,11 +66,19 @@
     // TIMED OUT 
         // increments timeout var by 1 
         // displays a screen telling you time is up 
+        // screen times out in 3 second 
         // checks if that was the last question 
             // if last: call results method 
             // else: call next question 
 
 
     // SHOW RESULTS 
+        // display the results screen with 
+            // correct 
+            // incorrect 
+            // timed out
+            // reset button that calls reset method
 
     // RESET GAME 
+        // reset all variables (tbd) 
+        // call get question method 
