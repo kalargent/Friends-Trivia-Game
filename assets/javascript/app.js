8e218b805e259012d5dd147d5c03c0071a5199b7
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-05-21 17:21:07 -0400

    basic HTML and JS Pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,52 @@
+// ON CLICK EVENT FOR START BUTTON TO GO AWAY 
+
+
+// GAME VARIABLE WITH METHODS
+// declare Game variable as an object with the following properties: 
+    // questions
+    // current question 
+    // correct answers 
+    // incorrect answers 
+    // timeouts 
+
+    // QUESTIONS OBJECT WHICH INCLUDES AN ARRAY OF 
+    // QUESTIONS
+        //question text 
+        // question answers (array)
+        // correct answer 
+
+    //METHODS 
+    // COUNTER
+        // set the counter 
+
+    // GET QUESTION 
+        // 
+
+    // NEXT QUESTION 
+        //
+
+    // CORRECT ANSWER 
+        //increments answer var by 1
+        // displays a screen saying you were correct and <xx> was right
+        // checks if that was the last question 
+            // if last: call results method 
+            // else: call next question 
+
+    // INCORRECT ANSWER 
+        //increments incorrect answer var by 1
+        // displays a screen saying you were correct and <xx> was right
+        // checks if that was the last question 
+            // if last: call results method 
+            // else: call next question 
+
+    // TIMED OUT 
+        // increments timeout var by 1 
+        // displays a screen telling you time is up 
+        // checks if that was the last question 
+            // if last: call results method 
+            // else: call next question 
+
+
+    // SHOW RESULTS 
+
+    // RESET GAME 
